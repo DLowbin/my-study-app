0725b7065ad32d675f01195e15b36f44a1af5c9f
Dmitry <neocd@me.com>
2022-07-12 23:50:47 +0300

    Second task preparation

---
+++ src/components/counter.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const Counter = (props) => {
+  // const [value, setValue] = useState(props.value);
+  const { value } = props;
+
+  const formatValue = () => {
+    return value === 0 ? <h1>Empty</h1> : <h3>{value}</h3>;
+  };
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+  const handleIncrement = () => {
+    // setValue((prevstate) => prevstate + 1);
+  };
+  const handleDecrement = () => {
+    // setValue((prevstate) => prevstate - 1);
+  };
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span className={getBadgeClasses()}>{formatValue()}</span>
+      <button
+        className="btn btn-primary btn-m m-2"
+        onClick={() => props.onIncrement()}
+      >
+        +
+      </button>
+      <button
+        className="btn btn-primary btn-m m-2"
+        onClick={() => props.onDecrement()}
+      >
+        -
+      </button>
+      <button
+        className="btn btn-danger btn-m m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/components/countersList.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 4, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initialState);
+
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((counter) => counter.id !== id);
+    setCounters(newCounters);
+  };
+
+  const handleIncrement = () => {};
+  const handleDecrement = () => {};
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
+      ))}
+    </>
+  );
+};
+
+export default CountersList;
